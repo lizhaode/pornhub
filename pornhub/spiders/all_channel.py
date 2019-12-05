3053e73acd5b9ade4339c827a953288fe5b3dd7f
lizhaode <308697716@qq.com>
2019-12-05 20:46:15 +0800

    fix get channel error

--- pornhub/spiders/all_channel.py
+++ pornhub/spiders/all_channel.py
@@ -46,7 +46,8 @@ class AllChannel(scrapy.Spider):
 
     def video_page(self, response: HtmlResponse):
         video_title = response.css('h1.title').css('span::text').extract_first()
-        video_channel = response.css('div.usernameWrap.clearfix').css('a::text').extract_first()
+        video_channel = response.css('div.video-actions-container').css('div.usernameWrap.clearfix').css(
+            'a::text').extract_first()
         js = response.css('div.video-wrapper').css('#player').css('script').extract_first()
         prepare_js = js.split('<script type="text/javascript">')[1].split('loadScriptUniqueId')[0]
         exec_js = 'function f(){' + prepare_js + 'if (typeof (quality_2160p) != "undefined") { return quality_2160p; ' \
